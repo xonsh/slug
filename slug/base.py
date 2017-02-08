1c27214d2e0c2c228f110c2ac801c277d79344c4
Jamie Bliss <astronouth7303@gmail.com>
2017-02-07 20:21:27 -0500

    Basic implementation of Process.

--- slug/base.py
+++ slug/base.py
@@ -2,53 +2,42 @@
 Base, non-system specific abstract implementations.
 """
 import os
+import subprocess
 __all__ = ('Process', 'Pipe', 'ProcessGroup')
 
 
 class Process:
     def __init__(self, cmd, *, stdin=None, stdout=None, stderr=None,
-                 environ=None, priority=None):
-        NotImplemented
+                 cwd=None, environ=None):
+        self.cmd = cmd
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.cwd = cwd
+        self.environ = environ
+        self._proc = None
+
 
     def signal(self, signal):
         """
         Send a request to the process
         """
-        NotImplemented
+        if self._proc:
+            self._proc.send_signal(signal)
 
     def terminate(self):
         """
         Forcibly quit the process
         """
-
-    @property
-    def priority(self):
-        """
-        The priority of the process. The priority ranges from 19 to -20, where
-        lower is more priority.
-
-        Not all priorities are available at all permissions.
-        """
-        return vars(self)['priority']
-
-    @priority.setter
-    def priority(self, value):
-        vars(self)['priority'] = value
-        if self.started:
-            self._set_priority(value)
-
-    def _set_priority(self, value):
-        """
-        Actually call out to the OS to change the priority.
-        """
-        NotImplemented
+        if self._proc:
+            self._proc.kill()
 
     @property
     def started(self):
         """
         Has the process started?
         """
-        NotImplemented
+        return self._proc is not None
 
     @property
     def status(self):
@@ -67,20 +56,29 @@ class Process:
         """
         The process identifier. None if the process hasn't started.
         """
-        NotImplemented
+        if self._proc is not None:
+            return self._proc.pid
+
+    @property
+    def return_code(self):
+        """
+        The return code of the process. None if it hasn't returned yet.
+        """
+        if self._proc is not None:
+            return self._proc.returncode
 
     def start(self):
         """
         Start the process.
         """
-        NotImplemented
+        self._proc = subprocess.Popen(
+            self.cmd, stdin=self.stdin, stdout=self.stdout, stderr=self.stderr,
+            cwd=self.cwd, environ=self.environ
+        )
 
-    def _virtual_start(self, pid):
-        """
-        Mark the process as started when something else somewhere does the
-        starting.
-        """
-        NotImplemented
+    def join(self):
+        if self._proc is not None:
+            self._proc.wait()
 
 
 class ProcessGroup:
