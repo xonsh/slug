596b062dae3a45227f45cd4ebbe66f8aa89cf677
Jamie Bliss <astronouth7303@gmail.com>
2017-02-14 17:48:25 -0500

    Additional tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -25,3 +25,39 @@ def test_pipe_output():
     proc.join()  # Commenting this out causes data to be None?
     assert proc.return_code == 0
     assert data == b'hello\n'
+
+
+
+def test_pipe_output():
+    pi = Pipe()
+    proc = Process([sys.executable, '-c', r'import sys; sys.exit(sys.stdin.readline() == "spam\n")'], stdin=pi.side_out)
+    proc.start()
+    pi.side_in.write(b"spam\n")
+    pi.side_in.close()
+    # Pipe is closed but process might still be live
+    proc.join()  # Commenting this out causes data to be None?
+    assert proc.return_code == 1
+
+
+def test_inner_pipe():
+    pi = Pipe()
+    producer = Process([sys.executable, '-c', r'print("eggs")'], stdout=pi.side_in)
+    consumer = Process([sys.executable, '-c', r'import sys; sys.exit(sys.stdin.readline() == "eggs\n")'], stdin=pi.side_out)
+    producer.start()
+    consumer.start()
+    producer.join()
+    consumer.join()
+    assert producer.return_code == 0
+    assert consumer.return_code == 1
+
+
+def test_inner_pipe_reversed_order():
+    pi = Pipe()
+    producer = Process([sys.executable, '-c', r'print("eggs")'], stdout=pi.side_in)
+    consumer = Process([sys.executable, '-c', r'import sys; sys.exit(sys.stdin.readline() == "eggs\n")'], stdin=pi.side_out)
+    consumer.start()
+    producer.start()
+    consumer.join()
+    producer.join()
+    assert producer.return_code == 0
+    assert consumer.return_code == 1
