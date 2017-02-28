2a363af849078b2e5023c0dbcda7dbb3aa97d8e4
Jamie Bliss <astronouth7303@gmail.com>
2017-02-27 20:38:50 -0500

    Add iterator at chunked pipe testing

--- tests/test_pipe.py
+++ tests/test_pipe.py
@@ -9,13 +9,15 @@ def test_goesthrough():
 
     data = p.side_out.read()
     assert data == b'Hello'
+    data = p.side_out.read()
+    assert data == b''
 
 
 def test_eof():
     p = Pipe()
     p.side_in.write(b"spam")
 
-    data = p.side_out.read()
+    data = p.side_out.read(4)  # read() or read(-1) read until EOF
     assert data == b'spam'
 
     p.side_in.close()
@@ -50,3 +52,28 @@ def test_iter_eof():
 
     with pytest.raises(StopIteration):
         next(riter)
+
+def test_chunk():
+    p = Pipe()
+    p.side_in.write(b"Hello")
+    p.side_in.close()
+
+    data = p.side_out.read(3)
+    assert data == b'Hel'
+    data = p.side_out.read(2)
+    assert data == b'lo'
+    data = p.side_out.read(5)
+    assert data == b''
+
+
+def test_chunk_eof():
+    p = Pipe()
+    p.side_in.write(b"Hello")
+
+    data = p.side_out.read(3)
+    assert data == b'Hel'
+    data = p.side_out.read(2)
+    assert data == b'lo'
+    p.side_in.close()
+    data = p.side_out.read(5)
+    assert data == b''
