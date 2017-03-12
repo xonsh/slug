d3d023d8eb10661a5febd0b9259bc38530b97208
Jamie Bliss <astronouth7303@gmail.com>
2017-03-12 15:42:50 -0400

    More complete testing of quickconnect

--- tests/test_quickconnect.py
+++ tests/test_quickconnect.py
@@ -1,3 +1,5 @@
+import pytest
+import time
 from slug import QuickConnect, Pipe
 
 
@@ -15,3 +17,53 @@ def test_through():
 
     roundtrip = pout.side_out.read()
     assert roundtrip == b'spameggs'
+
+
+# Until we implement Posix and #7
+@pytest.mark.xfail
+def test_change_input():
+    p1 = Pipe()
+    p2 = Pipe()
+    pout = Pipe()
+    qc = QuickConnect(
+        side_in=p1.side_out,
+        side_out=pout.side_in,
+        keepopen=False
+    )
+
+    p1.side_in.write(b'spam')
+    p2.side_in.write(b'foo')
+
+    time.sleep(1.0)
+    qc.side_in = p2.side_out
+
+    p1.side_in.write(b'eggs')
+    p2.side_in.write(b'bar')
+
+    p1.side_in.close()
+    p2.side_in.close()
+
+    roundtrip = pout.side_out.read()
+    assert roundtrip == b'spamfoobar'
+
+
+def test_change_output():
+    pin = Pipe()
+    p1 = Pipe()
+    p2 = Pipe()
+    qc = QuickConnect(
+        side_in=pin.side_out,
+        side_out=p1.side_in,
+    )
+
+    pin.side_in.write(b'spam')
+
+    time.sleep(1.0)
+    qc.side_out = p2.side_in
+
+    pin.side_in.write(b'eggs')
+
+    out1 = p1.side_out.read(4000)
+    out2 = p2.side_out.read(4000)
+    assert out1 == b'spam'
+    assert out2 == b'eggs'
