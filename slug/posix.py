e5939631835ce04d808246fdc391c95354f3b044
Jamie Bliss <astronouth7303@gmail.com>
2017-03-11 20:47:38 -0500

    Correct Valve behavior on Posix

--- slug/posix.py
+++ slug/posix.py
@@ -4,9 +4,10 @@ Versions of the base functionality optimized for by-the-spec POSIX.
 Linux/Mac/BSD-specific code should live elsewhere.
 """
 import signal
+import selectors
 from . import base
 
-__all__ = ('Process',)
+__all__ = ('Process', 'Valve')
 
 
 class Process(base.Process):
@@ -22,3 +23,33 @@ class Process(base.Process):
         Continue the process after it's been paused
         """
         self.signal(signal.SIGCONT)
+
+
+class Valve(base.Valve):
+    """
+    Forwards from one file-like to another, but this flow may be paused and
+    resumed.
+
+    This implementation doesn't support changing the target descriptors after
+    initialization.
+    """
+    def _thread(self):
+        sel = selectors.DefaultSelector()
+        sel.register(self.side_in, selectors.EVENT_READ)
+        while True:
+            sel.select()
+            # Don't care about the event, there's only one thing it can be.
+
+            # This feels like there's a race condition in here, but I think the
+            # window is small enough we can call it "slight asyncronousity".
+            if not self.gate.is_set():
+                self.gate.wait()
+                continue
+
+            chunk = self.side_in.read(self.CHUNKSIZE)
+            if chunk == b'':
+                break
+            else:
+                self.side_out.write(chunk)
+        if not self.keepopen:
+            self.side_out.close()
